fec50ca7c102b9226db64b035314a499a5f7c4dc
Christina  Matson <cmatson93@gmail.com>
2018-01-18 14:47:37 -0800

    Search bar spacing

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Row, Col } from 'reactstrap';
 import { Input } from 'reactstrap';
 import { Button } from 'reactstrap';
-// import "./SearchBar.css";
+import "./SearchBar.css";
 
 
 export default class SearchBar extends Component {
@@ -20,9 +20,8 @@ export default class SearchBar extends Component {
 	render() {
 		return (
 			<div className='searchbar '>
-                <Row className='search-bar-row justify-content-center'>
-                    <Col md='3'></Col>
-                    <Col className='col-search-location' md='4' xs='12'>
+                {/* <Row className='search-bar-row justify-content-center'>  */}
+                    <Col className='col-search-location'>
                         <Input
                             className="location-seach-input"
                             placeholder='Search Location'
@@ -30,11 +29,10 @@ export default class SearchBar extends Component {
                             value={this.props.location}
                         />
                     </Col>
-                    <Col className='col-search-btn' md='5' xs='3'>
-						<Button className="search-btn" onClick={this.props.buttonHandler}>Search</Button>
+                    <Col className='col-search-btn'>
+						<Button className="btn" onClick={this.props.buttonHandler}>Search</Button>
 					</Col>
-                    <Col md='2'></Col>
-				</Row>
+				{/* </Row>  */}
 			</div>
 		);
 	}
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -8,13 +8,15 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="home">
-			<Jumbotron fluid className="jumbotron">
-				<Container fluid className="container">
-					<h1>HOME PAGE</h1>
-						<SearchBar className="searchBar"/>
-				</Container>
-			</Jumbotron>
+				<Jumbotron fluid className="jumbotron">
+					<Container fluid className="container">
+						<h1>HOME PAGE</h1>
+							<SearchBar className="searchBar"/>
+					</Container>
+				</Jumbotron>
+				
 			</div>
+				
 		)
 	}
 }
