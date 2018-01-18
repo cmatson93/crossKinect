ac76ff79a082e2bd3888051698df4110caff503b
Christina  Matson <cmatson93@gmail.com>
2018-01-18 15:44:27 -0800

    Moved search button to center

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,57 +5,6 @@ import API from "../../utils/API";
 import { Link } from "react-router-dom";
 
 class Home extends Component {
-	state = {
-		churches: [],
-		name: "",
-		location: "",
-		summary: "",
-		query: this.props.match.params || {}
-	}
-
-	checkState = () => {
-		let searchObj = {};
-		if (this.state.name.length > 0) {
-			searchObj.name = this.state.name;
-		}
-		if (this.state.location.length > 0) {
-			searchObj.location = this.state.location;
-		}
-		return searchObj;
-	}
-
-	handleInputChange = event => {
-		console.log("-");
-		// Destructure the name and value properties off of event.target
-		// Update the appropriate state
-		const { name, value } = event.target;
-		this.setState({
-			[name]: value
-		});
-		console.log(this.state);
-	}
-
-	handleFormSubmit = event => {
-		console.log("Button pushed");
-		console.log(this.state.location);
-		event.preventDefault();
-		if (this.state.location) {
-			API.getChurches({
-				location: this.state.location
-			})
-				.then(res => console.log(res.data))
-				//this.setState({churches: res.data , name: "", location: "", summary: ""}) ;
-				.catch(err => console.log(err));
-		}
-	}
-
-	loadBooks = () => {
-		API.getChurches()
-			.then(res =>
-				this.setState({ churches: res.data, name: "", location: "", summary: "" })
-			)
-			.catch(err => console.log(err));
-	}
 
 	render() {
 		return (
