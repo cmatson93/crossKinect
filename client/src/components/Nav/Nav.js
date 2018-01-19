fb31daf5d3a0b3342276bcc3c33082d9d2a2476b
Christina  Matson <cmatson93@gmail.com>
2018-01-19 13:40:51 -0800

    Nav Bar additions

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -14,7 +14,56 @@ const Nav = () =>
           Cross Kinect
         </a>
       </div>
+      <div className="nav-links">
+        <div className="chruches-link">
+          <button type="button" className="collapsed navbar-toggle">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar" /> <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <a href="" className="navbar-brand">
+            Churches 
+          </a>
+        </div>
+        <div className="events-link">
+          <button type="button" className="collapsed navbar-toggle">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar" /> <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <a href="" className="navbar-brand">
+            Events
+          </a>
+        </div>
+        <div className="login/signup-link">
+          <button type="button" className="collapsed navbar-toggle">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar" /> <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <a href="" className="navbar-brand">
+            Sign Up
+          </a>
+        </div>
+      </div>
     </div>
   </nav>;
 
 export default Nav;
+
+
+
+{/* <ul>
+  <li class="nav-item ">
+    <a class="nav-link active " href="">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="">Events</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Churches</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="">Sign Up</a>
+  </li>
+</ul> */}
